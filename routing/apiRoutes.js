ff9a3f42044e659cf02626e04408a25fe7bc8700
David Estlund <destlund@gmail.com>
2018-02-06 17:44:57 -0600

    It's POSTing but it's misinterpreting some stuff...

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -2,6 +2,17 @@
 
 // a GET route with the url /api/friends
 module.exports = function (app) {
+    // set up the friends array
+    var friends = [];
+    function Friend(name, photo, scores) {
+        this.name = name;
+        this.photo = photo;
+        this.scores = scores;
+        friends.push(this)
+    }
+    var Ahmed = new Friend("Ahmed", "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg", [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]);
+    var David = new Friend("David", "myUrl", [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
     app.get("/api/friends", function (req, res) {
         res.json(friends);
     });
@@ -10,6 +21,22 @@ module.exports = function (app) {
     app.post("/api/friends", function (req, res) {
         friends.push(req.body);
         res.json(true);
+        console.log(friends);
+
+        // here's where it gets tricksy with the compatability logic:
+        var myIndex = friends.length - 1;
+        var myData = friends[myIndex];
+        var compatibilityData = [];
+        var compatabilityScores = [];
+        for (i = 0; i < myIndex; i++) {
+            for (n = 0; n < myData.scores[n]; n++) {
+                var compatabilityPoint = friends[i].scores[n] - myData.scores[n];
+                compatabilityData.push(compatabilityPoint);
+            }
+            var compatabilityScore = Math.abs(sum(compatabilityData));
+            compatabilityScores.push(compatabilityScore);
+        }
+        console.log(compatabilityScores);
     });
 };
 
